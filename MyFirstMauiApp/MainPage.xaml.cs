74189b4521cbf2fe0cb84846f7f28fe370aba446
DamjanT1988 <Damjan_t@hotmail.com>
2022-12-07 18:48:56 +0100

    1

--- MyFirstMauiApp/MainPage.xaml.cs
+++ MyFirstMauiApp/MainPage.xaml.cs
@@ -1,24 +1,16 @@
-﻿namespace MyFirstMauiApp;
+﻿using MyFirstMauiApp.ViewModel;
+
+namespace MyFirstMauiApp;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
-	public MainPage()
+	public MainPage(MainViewModel vm)
 	{
 		InitializeComponent();
-	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count += 10;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
+		//13
+		BindingContext = vm;
 	}
 }
+
 
---
+++ MyFirstMauiApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,70 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace MyFirstMauiApp.ViewModel;
+
+//12 - create and fill the bindings
+public partial class MainViewModel : ObservableObject
+{
+
+    public MainViewModel()
+    {   //bind #2
+        Items = new ObservableCollection<string>();
+    }
+
+    [ObservableProperty]
+    ObservableCollection<string> items;
+
+    [ObservableProperty]
+    //bind #1
+    string text;
+
+    [RelayCommand]
+
+    //method
+    void Add()
+    {
+        if (string.IsNullOrWhiteSpace(Text))
+            return;
+
+        Items.Add(Text);
+        //add item
+        Text = string.Empty;
+    }
+
+    [RelayCommand]
+    void Delete(string s)
+    {
+        if(Items.Contains(s))
+        {
+            Items.Remove(s);
+        }
+    }
+}
+
+
+
+/*
+public class MainViewModel : INotifyPropertyChanged
+{
+    string text;
+
+    public string Text
+    {
+        get => text;
+        set
+        {
+            text = value;
+            OnPropertyChanged(nameof(Text));
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    void OnPropertyChanged(string name) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+}
+*/
